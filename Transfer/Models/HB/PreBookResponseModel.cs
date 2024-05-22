53618618684f745cf60d4711376bf169b746e192
Rakesh <rakeshk@bookingexpress.co>
2024-05-22 10:43:04 +0000

    update

---
+++ Transfer/Models/HB/PreBookResponseModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TravillioXMLOutService.Transfer.Models.HB
+{
+    public class PreBookResponseModel
+    {
+
+        public Services services { get; set; }
+
+    }
+}
